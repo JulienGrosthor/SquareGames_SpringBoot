5f576ee3ae6827d29272f4bbc66c807e6ddd9c60
julien.grosthor <julien.grosthor@le-campus-numerique.fr>
2025-01-08 11:54:49 +0100

    added DAO H2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO GameUser VALUES (1, 'Pepe the Frog', 'pepe@frog.chan');
+
+INSERT INTO GameUser VALUES (2, 'TechnoViking', 'techno@viking.com');
+
+INSERT INTO GameUser VALUES (3, 'Gondola', 'gond@ola.chan');

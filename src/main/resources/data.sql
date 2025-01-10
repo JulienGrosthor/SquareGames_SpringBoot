84dbe2080a5aea47de9b5e34ee6bfcdf1f285f49
julien.grosthor <julien.grosthor@le-campus-numerique.fr>
2025-01-10 16:38:32 +0100

    final commit, goodbye world

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,5 @@ INSERT INTO GameUser VALUES (1, 'Pepe the Frog', 'pepe@frog.chan');
 INSERT INTO GameUser VALUES (2, 'TechnoViking', 'techno@viking.com');
 
 INSERT INTO GameUser VALUES (3, 'Gondola', 'gond@ola.chan');
+
+INSERT INTO GameUser VALUES (4, 'Ibram Gaunt', 'ibram@gaunt.com');
